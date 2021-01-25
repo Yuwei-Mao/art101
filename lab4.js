ecd229ca74804df05cf93d86f001195c1e2c196c
Yuwei Mao <ymao30@ucsc.edu>
2021-01-24 19:36:29 -0800

    1

---
+++ lab4.js
@@ -0,0 +1,9 @@
+// Create a random number between 1 and 100
+// Loop until the user win or exceed the limit times of guess
+  //Input user's guess
+  //If user's guess equal to the random number
+    //Print user is win and exit the Loop
+  //Elseif user's guess is bigger than the random number
+    //Print user's guess is bigger than the random number
+  //Else print user's guess is smaller than the random number
+  //Increase times of guess plus by 1
