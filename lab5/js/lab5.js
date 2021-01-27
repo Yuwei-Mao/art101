b7856f84b782892ac5f7959d357bf36bf0f8cd2e
Yuwei Mao <ymao30@ucsc.edu>
2021-01-26 22:22:32 -0800

    1

---
+++ lab5/js/lab5.js
@@ -0,0 +1,22 @@
+/*
+ * Author: Mengya Qiu and Yuwei Mao
+ * Created: 1 January
+ * License: Public Domain
+ */
+
+ // Define Variables
+var make = "BMW";
+var model = "523Li";
+var color = "Brown";
+var year = 2008;
+var ownIt = true;
+
+// Calulate
+var age = 2019 - year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own it: " + ownIt + "<br>");
